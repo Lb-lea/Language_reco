5d94e4c853a0cde1698597e7d29395261ddaf9a6
Le-Leah <happyvivus@gmail.com>
2017-01-29 16:54:11 +0100

    tests

--- Langreco/test/langReco/reco/testMyLanguageRecognizer1test2.java
+++ Langreco/test/langReco/reco/testMyLanguageRecognizer1test2.java
@@ -7,20 +7,23 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 
 /**
- * Created by E154981H on 20/01/17.
+ * Created by Lea on 29/01/2017.
  */
-public class MyLanguageRecognizer2Test1 {
+public class testMyLanguageRecognizer1test2{
+
+
     @Test
-    public void testMyLanguageRecognizer2() {
+    public void testMyLanguageRecognizer1test2() {
         String cc = "-20";
         String goldSentPath = "data/gold/gold-sent"+cc+".txt";
         String goldLangPath = "data/gold/gold-lang"+cc+".txt";
 
-        //test with
-        LanguageRecognizer my1 = new MyLanguageRecognizer2("lm/ULTIME_CONFIG.txt");
 
+        LanguageRecognizer my1 = new MyLanguageRecognizer1("lm/fichConfig_trigram-100.txt");
+        // or use the following if you want to consider all the languages
+        // LanguageRecognizer baseline = new BaselineLanguageRecognizer();
 
-        String hypLangFilePath = "data/gold/gold-test-unk"+cc+".txt";
+        String hypLangFilePath = "data/gold/gold-test-tri"+cc+".txt";
         my1.recognizeFileLanguage(goldSentPath, hypLangFilePath);
         System.out.printf("System performance = %f\n", Performance.evaluate(goldLangPath, hypLangFilePath));
     }
@@ -35,4 +38,7 @@ public class MyLanguageRecognizer2Test1 {
     {
         System.out.println("\n=== " + name.getMethodName() + " =====================");
     }
+
 }
+
+
