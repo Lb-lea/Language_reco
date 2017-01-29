77a62382e8717f7e62c2dd9d93d3b429d18e5067
W4ldschr31n <nicolasrich@hotmail.fr>
2017-01-29 16:14:16 +0100

    test LaPlace

--- LangModel/test/langModel/MyLaplaceLanguageTest.java
+++ LangModel/test/langModel/MyLaplaceLanguageTest.java
@@ -34,8 +34,13 @@ public class MyLaplaceLanguageTest {
     @Test
     public void testSentenceProb() {
         MyLaplaceLanguageModel nlm = this.set();
-        //System.out.println(nlm.getSentenceProb("<s> Ce cours est la </s>"));
-        assertEquals(0.008,(double)nlm.getSentenceProb("<s> Ce cours est la </s>"),0.001);//dela =precision
+        assertEquals((double)5/42, (double) nlm.getNgramProb("<s>"), 0.001);
+        assertEquals((double)2/25, (double) nlm.getNgramProb("<s> Ce"), 0.001);
+        assertEquals((double)2/22, (double) nlm.getNgramProb("Ce cours"), 0.001);
+        assertEquals((double)2/22, (double) nlm.getNgramProb("cours est"), 0.001);
+        assertEquals((double)2/24, (double) nlm.getNgramProb("est la"), 0.001);
+        assertEquals((double)2/22, (double) nlm.getNgramProb("la </s>"), 0.001);
+        assertEquals((double)(320/268329600),(double)nlm.getSentenceProb("<s> Ce cours est la </s>"),0.001);//dela =precision
     }
 
 }
